75643d343cd2b06704b1e1f31e6073de89c6c205
cubefriendly <david@roon.me>
2015-06-23 19:16:49 +0200

    toMetric transformer

--- src/main/java/org/cubefriendly/engine/cube/CubeData.java
+++ src/main/java/org/cubefriendly/engine/cube/CubeData.java
@@ -47,14 +47,22 @@ public class CubeData {
     }
 
     public CubeData toMetric(int index) {
+        int before = sizes.size();
         sizes.remove(index);
+        int after = sizes.size();
+
+        if(after + 1 != before){
+            throw new RuntimeException("sizes removing the wrong way " + before + " " + after);
+        }
         BTreeMap<int[], String[]> dest = db.treeMapCreate("dest").comparator(Fun.INT_ARRAY_COMPARATOR).makeOrGet();
 
         for(Map.Entry<int[],String[]> entry : data.entrySet()){
             int[] newKey = new int[sizes.size()];
-            System.arraycopy(entry.getKey(),0,newKey,0,index);
-            System.arraycopy(entry.getKey(),index + 1,newKey,index,entry.getKey().length - index - 1);
-            dest.put(newKey,entry.getValue());
+            System.arraycopy(entry.getKey(), 0, newKey, 0, index);
+            if(index < entry.getKey().length -1){
+                System.arraycopy(entry.getKey(),index + 1,newKey,index,entry.getKey().length - index - 1);
+            }
+            dest.put(newKey, entry.getValue());
         }
         data.clear();
         db.commit();
